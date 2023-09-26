86ba18dc9177c9d1c034b5a074a0424748fe78ad
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-26 12:03:02 +0200

    Spring Boot REST API with Request Param - @RequestParam

--- src/main/java/com/dgmf/bean/Student.java
+++ src/main/java/com/dgmf/bean/Student.java
@@ -1,6 +1,5 @@
 package com.dgmf.bean;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/com/dgmf/controller/StudentController.java
+++ src/main/java/com/dgmf/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.dgmf.controller;
 import com.dgmf.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -55,12 +56,29 @@ public class StudentController {
     // HTTP GET Request : http://localhost:8080/students/1/John/Doe
     // {id}, {first-name} and {last-name} ==> URI Template variables
     @GetMapping("/students/{id}/{first-name}/{last-name}")
-
-    //
     public Student studentPathVariable(
             @PathVariable("id") Long studentId,
             @PathVariable("first-name") String studentFirstName,
             @PathVariable("last-name") String studentLastName
+    ) {
+        Student student = Student.builder()
+                .id(studentId)
+                .firstName(studentFirstName)
+                .lastName(studentLastName)
+                .build();
+
+        return student;
+    }
+
+    // Rest API that handles GET Request
+    // Rest API with Request Params
+    // GET Request :
+    // http://localhost:8080/students/query?id=1&firstname=Franck&lastname=Dodo
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(
+            @RequestParam("id") Long studentId,
+            @RequestParam("firstname") String studentFirstName,
+            @RequestParam("lastname") String studentLastName
     ) {
         Student student = Student.builder()
                 .id(studentId)
